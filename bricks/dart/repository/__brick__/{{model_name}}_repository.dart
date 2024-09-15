d2737d0e284fff354e1c05a2d9f62e0cffb0ae5e
andikadeveloper <mandikadev@gmail.com>
2024-09-15 19:28:08 +0700

    chore: fix dart repository brick

--- bricks/dart/repository/__brick__/{{model_name}}_repository.dart
+++ bricks/dart/repository/__brick__/{{model_name}}_repository.dart
@@ -2,15 +2,10 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import '../models/{{model_name}}.dart';
 
 class {{model_name.pascalCase()}}Repository {
-
   Future<List<{{model_name.pascalCase()}}>> findAll({required int id}) async {
-    final response = await Supabase.instance.client
-        .from('{{table_name}}')
-        .select();
-    if (response.error != null) {
-      throw response.error!;
-    }
-    return response.data!.map((e) => {{model_name.pascalCase()}}.fromJson(e)).toList();
+    final response = await Supabase.instance.client.from('{{table_name}}').select();
+
+    return response.map((e) => {{model_name.pascalCase()}}.fromJson(e)).toList();
   }
 
   Future<{{model_name.pascalCase()}}> findOne({required int id}) async {
@@ -19,41 +14,22 @@ class {{model_name.pascalCase()}}Repository {
         .select()
         .eq('id', id)
         .single();
-    if (response.error != null) {
-      throw response.error!;
-    }
-    return {{model_name.pascalCase()}}.fromJson(response.data);
+
+    return {{model_name.pascalCase()}}.fromJson(response);
   }
 
-  Future<{{model_name.pascalCase()}}> create({required {{model_name.pascalCase()}} {{model_name}}}) async {
-    final response = await Supabase.instance.client
-        .from('{{table_name}}')
-        .upsert({{model_name}}.toJson());
-    if (response.error != null) {
-      throw response.error!;
-    }
-    return {{model_name.pascalCase()}}.fromJson(response.data);
+  Future<void> create({required {{model_name.pascalCase()}} {{model_name}}}) async {
+    await Supabase.instance.client.from('{{table_name}}').insert({{model_name}}.toJson());
   }
 
-  Future<{{model_name.pascalCase()}}> update({required {{model_name.pascalCase()}} {{model_name}}}) async {
-    final response = await Supabase.instance.client
+  Future<void> update({required {{model_name.pascalCase()}} {{model_name}}}) async {
+    await Supabase.instance.client
         .from('{{table_name}}')
         .update({{model_name}}.toJson())
         .eq('id', {{model_name}}.id);
-    if (response.error != null) {
-      throw response.error!;
-    }
-    return {{model_name.pascalCase()}}.fromJson(response.data);
   }
 
-  Future<{{model_name.pascalCase()}}> delete({required int id}) async {
-    final response = await Supabase.instance.client
-        .from('{{table_name}}')
-        .delete()
-        .eq('id', id);
-    if (response.error != null) {
-      throw response.error!;
-    }
-    return {{model_name.pascalCase()}}.fromJson(response.data);
+  Future<void> delete({required int id}) async {
+    await Supabase.instance.client.from('{{table_name}}').delete().eq('id', id);
   }
 }
